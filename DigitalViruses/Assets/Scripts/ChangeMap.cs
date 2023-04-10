574b2d7fcba6ce36292dc0ed831114944a6c75ca
Rtemmmm <yadolbaeb30@gmail.com>
2023-04-10 22:14:38 +0500

    version 2.1

--- DigitalViruses/Assets/Scripts/ChangeMap.cs
+++ DigitalViruses/Assets/Scripts/ChangeMap.cs
@@ -1,20 +1,36 @@
+using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
+
+public class Country
+{
+    public string Name { get; }
+    public int PointCount { get; }
+    
+    public Vector3 Coordinate { get; set; }
+
+    public (float, float) X, Y;
+
+    public Country(string name, int pointCount, (float, float) x, (float, float) y)
+    {
+        Name = name;
+        PointCount = pointCount;
+        X = x;
+        Y = y;
+    }
+
+    public Vector3 GetRandomCoordinate()
+    {
+        return new Vector3(Random.Range(X.Item1, X.Item2), Random.Range(Y.Item1, Y.Item2));
+    }
+}
 
 public class ChangeMap : MonoBehaviour
 {
     [SerializeField] private GameObject point;
     [SerializeField] private Transform parent;
 
-    private GameObject _northAmericaPoint;
-    private GameObject _southAmericaPoint;
-    private GameObject _greenLandPoint;
-    private GameObject _iceLandPoint;
-    private GameObject _africaPoint;
-    private GameObject _russiaPoint;
-    private GameObject _japanPoint;
-    private GameObject _australiaPoint;
-
+    private GameObject _point;
 
     [Header("Количество точек")] 
     [SerializeField] private int pointCountNorthAmerica = 2000;
@@ -31,116 +47,67 @@ public class ChangeMap : MonoBehaviour
         CreatePoints();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         DeletePoints();
     }
+    
 
     private void CreatePoints()
     {
-        InstantiateNorthAmerica();
-        InstantiateSouthAmerica();
-        InstantiateGreenLand();
-        InstantiateIceLand();
-        InstantiateAfrica();
-        InstantiateRussia();
-        InstantiateJapan();
-        InstantiateAustralia();
+        var countries = new List<Country>
+        {
+            new ("NorthAmerica", pointCountNorthAmerica, 
+                (-9f, -4.25f), (0f, 5.3f)),
+            
+            new ("SouthAmerica", pointCountSouthAmerica, 
+                (-5.80f, -3f), (0f, -4.60f)),
+            
+            new ("SouthAmerica", pointCountSouthAmerica, 
+                (-5.80f, -3f), (0f, -4.60f)),
+            
+            new ("GreenLand", pointCountGreenLand, 
+                (-4.36f, -2.27f), (3.28f, 5.30f)),
+            
+            new ("IceLand", pointCountIceLand, 
+                (-2.40f, -1.85f), (3.80f, 4.25f)),
+            
+            new ("Africa", pointCountAfrica, 
+                (-2.15f, 2.10f), (-3f, 1f)),
+            
+            new ("Russia", pointCountRussia, 
+                (-1.65f, 9.45f), (1f, 5.30f)),
+            
+            new ("Japan", pointCountJapan, 
+                (4.23f, 6f), (-1.5f, 1f)),
+            
+            new ("Australia", pointCountAustralia, 
+                (5.40f, 7.60f), (-3.80f, -1.50f)),
+        };
+
+        foreach (var country in countries)
+        {
+            InstantiatePoints(country);
+        }
     }
 
     private void DeletePoints()
     {
-        if (parent.transform.childCount <= 0) 
+        if (parent.childCount <= 0) 
             return;
         
-        var randomIndex = Random.Range(0, parent.transform.childCount);
-        Destroy(parent.transform.GetChild(randomIndex).gameObject);
+        var randomIndex = Random.Range(0, parent.childCount);
+        Destroy(parent.GetChild(randomIndex).gameObject);
     }
     
-    private void InstantiateNorthAmerica()
+    private void InstantiatePoints(Country country)
     {
-        for (var i = 0; i < pointCountNorthAmerica; i++)
+        for (var i = 0; i < country.PointCount; i++)
         {
-            _northAmericaPoint = Instantiate(point, 
-                new Vector3(Random.Range(-9f, -4.25f), Random.Range(0f, 5.3f)), 
+            _point = Instantiate(point, 
+                country.GetRandomCoordinate(), 
                 Quaternion.identity);
-            _northAmericaPoint.transform.parent = parent;
-        }
-    }
-    
-    private void InstantiateSouthAmerica()
-    {
-        for (var i = 0; i < pointCountSouthAmerica; i++)
-        {    _southAmericaPoint = Instantiate(point, 
-                new Vector3(Random.Range(-5.80f, -3f), Random.Range(0f, -4.60f)), 
-                Quaternion.identity);        
-            _southAmericaPoint.transform.parent = parent;
-        }
-    }
-    
-    private void InstantiateGreenLand()
-    {
-        for (var i = 0; i < pointCountGreenLand; i++)
-        {
-            _greenLandPoint = Instantiate(point,
-                new Vector3(Random.Range(-4.36f, -2.27f), Random.Range(3.28f, 5.30f)),
-                Quaternion.identity);
-            _greenLandPoint.transform.parent = parent;
-        }
-    }
-    
-    private void InstantiateIceLand()
-    {
-        for (var i = 0; i < pointCountIceLand; i++)
-        {
-            _iceLandPoint = Instantiate(point,
-                new Vector3(Random.Range(-2.40f, -1.85f), Random.Range(3.80f, 4.25f)),
-                Quaternion.identity);
-            _iceLandPoint.transform.parent = parent;
-        }
-    }
-    
-    private void InstantiateAfrica()
-    {
-        for (var i = 0; i < pointCountAfrica; i++)
-        {
-            _africaPoint = Instantiate(point,
-                new Vector3(Random.Range(-2.15f, 2.10f), Random.Range(-3f, 1f)),
-                Quaternion.identity);
-            _africaPoint.transform.parent = parent;
-        }
-    }
-    
-    private void InstantiateRussia()
-    {
-        for (var i = 0; i < pointCountRussia; i++)
-        {
-            _russiaPoint = Instantiate(point,
-                new Vector3(Random.Range(-1.65f, 9.45f), Random.Range(1f, 5.30f)),
-                Quaternion.identity);
-            _russiaPoint.transform.parent = parent;
-        }
-    }
-    
-    private void InstantiateJapan()
-    {
-        for (var i = 0; i < pointCountJapan; i++)
-        {
-            _japanPoint = Instantiate(point,
-                new Vector3(Random.Range(4.23f, 6f), Random.Range(-1.5f, 1f)),
-                Quaternion.identity);
-            _japanPoint.transform.parent = parent;
-        }
-    }
-    
-    private void InstantiateAustralia()
-    {
-        for (var i = 0; i < pointCountAustralia; i++)
-        {
-            _australiaPoint = Instantiate(point,
-                new Vector3(Random.Range(5.40f, 7.60f), Random.Range(-3.80f, -1.50f)),
-                Quaternion.identity);
-            _australiaPoint.transform.parent = parent;
+            _point.transform.parent = parent;
         }
     }
 }
