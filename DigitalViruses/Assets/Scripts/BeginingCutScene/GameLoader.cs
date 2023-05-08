5bb23e545607587096a2982455bdef76ba23eec8
Rtemmmm <yadolbaeb30@gmail.com>
2023-05-08 23:32:40 +0500

    version 3.2
    +комикс другой(видео)

--- DigitalViruses/Assets/Scripts/BeginingCutScene/GameLoader.cs
+++ DigitalViruses/Assets/Scripts/BeginingCutScene/GameLoader.cs
@@ -3,7 +3,5 @@ using UnityEngine.SceneManagement;
 
 public class GameLoader : MonoBehaviour
 {
-    private void Start() => Invoke(nameof(LoadMainMenu), 2f);
-
-    private void LoadMainMenu() => SceneManager.LoadScene(1);
+    private void Start() => SceneManager.LoadScene(1);
 }
