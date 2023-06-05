4f018320a6ab6023dfac50aca75499775a6104c9
Rtemmmm <yadolbaeb30@gmail.com>
2023-06-05 18:46:52 +0500

    version 4.1
    +настройки в уровнях

--- DigitalViruses/Assets/Scripts/Game/Game.cs
+++ DigitalViruses/Assets/Scripts/Game/Game.cs
@@ -5,11 +5,15 @@ using UnityEngine.UI;
 public class Game : MonoBehaviour
 {
     [SerializeField] private Text moneyText;
+    [SerializeField] private AudioSource audioSource;
     public static int Money;
 
     private void Start()
     {
         Money = 30;
+        
+        audioSource.volume = PlayerPrefs.HasKey("VolumeSettingPreference")
+            ? PlayerPrefs.GetFloat("VolumeSettingPreference") : 0.05f;
     }
 
     private void Update()
